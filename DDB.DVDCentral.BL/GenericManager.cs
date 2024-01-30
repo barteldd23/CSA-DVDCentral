b5fe2004cfc488ed4f3797664923f71502c51f96
Dean Bartel <300089145@fvtc.edu>
2024-01-30 14:19:57 -0600

    Started fixing BL

---
+++ DDB.DVDCentral.BL/GenericManager.cs
@@ -0,0 +1,48 @@
+﻿using DDB.DVDCentral.PL2.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDB.DVDCentral.BL
+{
+    public abstract class GenericManager <T> where T : class
+    {
+        protected DbContextOptions<DVDCentralEntities> options;
+
+        public GenericManager(DbContextOptions<DVDCentralEntities> options)
+        {
+            this.options = options;
+        }
+
+        public List<T> Load()
+        {
+            return null;
+        }
+
+        public T LoadById(Guid id)
+        {
+            return null;
+        }
+
+        public int Insert(T entity, bool rollback = false)
+        {
+            return 0;
+        }
+
+        public int Update(T entity, bool rollback = false)
+        {
+            return 0;
+        }
+
+        public int Delete(Guid id, bool rollback = false)
+        {
+            return 0;
+        }
+
+
+
+    }
+}
