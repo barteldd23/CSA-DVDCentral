5ff675f4ed27fbf2a56c8a231b1f13815a395879
Dean Bartel <300089145@fvtc.edu>
2024-02-08 09:24:20 -0600

    Trying to connect ut's to managers

--- DDB.DVDCentral.BL.Test/GlobalUsings.cs
+++ DDB.DVDCentral.BL.Test/GlobalUsings.cs
@@ -1,3 +1,7 @@
+global using DDB.DVDCentral.BL.Models;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using DDB.DVDCentral.BL;
-global using DDB.DVDCentral.BL.Test;
+global using DDB.DVDCentral.PL2.Data;
+global using DDB.DVDCentral.PL2.Entities;
