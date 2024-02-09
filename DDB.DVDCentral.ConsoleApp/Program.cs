79e355e5d56d205afca8ddb3e89885fc57c5def5
Dean Bartel <300089145@fvtc.edu>
2024-02-09 12:50:54 -0600

    Added Console SignalR project

---
+++ DDB.DVDCentral.ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using DDB.DVDCentral.ConsoleApp;
+
+internal class Program
+{
+    private static string DrawMenu()
+    {
+        Console.WriteLine("Which operation do you wish to perform?");
+        Console.WriteLine("Connect to a channel (c)");
+        Console.WriteLine("Send a message to the channel (s)");
+        Console.WriteLine("Exit (x)");
+
+        string operation = Console.ReadLine();
+        return operation;
+    }
+
+
+    private static void Main(string[] args)
+    {
+        string user = "Bartel";
+        string hubAddress = "https://fvtcdp.azurewebsites.net/GameHub";
+        string operation = DrawMenu();
+
+        var signalRConnection = new SignalRConnection(hubAddress);
+
+        while (operation != "x")
+        {
+            switch (operation)
+            {
+                case "c":
+                    signalRConnection.ConnectToChannel(user);
+                    break;
+                case "s":
+                    break;
+                case "x":
+                    break;
+            }
+            operation = DrawMenu();
+        }
+    }
+}
