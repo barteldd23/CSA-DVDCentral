e5ee0a06bdcfffcd9cc3d1d1969ffae0af249109
Dean Bartel <300089145@fvtc.edu>
2024-02-22 14:41:58 -0600

    Test

--- DDB.DVDCentral.API/Hubs/BingoHub.cs
+++ DDB.DVDCentral.API/Hubs/BingoHub.cs
@@ -4,7 +4,7 @@ namespace DDB.DVDCentral.API.Hubs
 {
     public class BingoHub : Hub
     {
-
+        // .
         // Can have as many parameters as you need.
         public async Task SendMessage(string user, string message)
         {
